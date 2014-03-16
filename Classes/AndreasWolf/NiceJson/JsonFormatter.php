4c60e1d57b954eceb4e55e47845053ab6140e7c9
Andreas Wolf <aw@foundata.net>
2014-03-16 17:52:56 +0100

    [TASK] Change code indentation to tabs

--- Classes/AndreasWolf/NiceJson/JsonFormatter.php
+++ Classes/AndreasWolf/NiceJson/JsonFormatter.php
@@ -6,72 +6,72 @@ namespace AndreasWolf\NiceJson;
 
 
 class JsonFormatter {
-  /**
-  * Format a flat JSON string to make it more human-readable
-  *
-  * @param string $json The original JSON string to process
-  *        When the input is not a string it is assumed the input is RAW
-  *        and should be converted to JSON first of all.
-  * @return string Indented version of the original JSON string
-  */
-  public static function json_format($json) {
-    if (!is_string($json)) {
-      if (phpversion() && phpversion() >= 5.4) {
-        return json_encode($json, JSON_PRETTY_PRINT);
-      }
-      $json = json_encode($json);
-    }
-    $result      = '';
-    $pos         = 0;               // indentation level
-    $strLen      = strlen($json);
-    $indentStr   = "\t";
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
+	/**
+	 * Format a flat JSON string to make it more human-readable
+	 *
+	 * @param string $json The original JSON string to process
+	 *        When the input is not a string it is assumed the input is RAW
+	 *        and should be converted to JSON first of all.
+	 * @return string Indented version of the original JSON string
+	 */
+	public static function json_format($json) {
+		if (!is_string($json)) {
+			if (phpversion() && phpversion() >= 5.4) {
+				return json_encode($json, JSON_PRETTY_PRINT);
+			}
+			$json = json_encode($json);
+		}
+		$result      = '';
+		$pos         = 0;               // indentation level
+		$strLen      = strlen($json);
+		$indentStr   = "\t";
+		$newLine     = "\n";
+		$prevChar    = '';
+		$outOfQuotes = true;
 
-    for ($i = 0; $i < $strLen; $i++) {
-      // Grab the next character in the string
-      $char = substr($json, $i, 1);
+		for ($i = 0; $i < $strLen; $i++) {
+			// Grab the next character in the string
+			$char = substr($json, $i, 1);
 
-      // Are we inside a quoted string?
-      if ($char == '"' && $prevChar != '\\') {
-        $outOfQuotes = !$outOfQuotes;
-      }
-      // If this character is the end of an element,
-      // output a new line and indent the next line
-      else if (($char == '}' || $char == ']') && $outOfQuotes) {
-        $result .= $newLine;
-        $pos--;
-        for ($j = 0; $j < $pos; $j++) {
-          $result .= $indentStr;
-        }
-      }
-      // eat all non-essential whitespace in the input as we do our own here and it would only mess up our process
-      else if ($outOfQuotes && false !== strpos(" \t\r\n", $char)) {
-        continue;
-      }
+			// Are we inside a quoted string?
+			if ($char == '"' && $prevChar != '\\') {
+				$outOfQuotes = !$outOfQuotes;
+			}
+			// If this character is the end of an element,
+			// output a new line and indent the next line
+			else if (($char == '}' || $char == ']') && $outOfQuotes) {
+				$result .= $newLine;
+				$pos--;
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+			// eat all non-essential whitespace in the input as we do our own here and it would only mess up our process
+			else if ($outOfQuotes && false !== strpos(" \t\r\n", $char)) {
+				continue;
+			}
 
-      // Add the character to the result string
-      $result .= $char;
-      // always add a space after a field colon:
-      if ($char == ':' && $outOfQuotes) {
-        $result .= ' ';
-      }
+			// Add the character to the result string
+			$result .= $char;
+			// always add a space after a field colon:
+			if ($char == ':' && $outOfQuotes) {
+				$result .= ' ';
+			}
 
-      // If the last character was the beginning of an element,
-      // output a new line and indent the next line
-      if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-        $result .= $newLine;
-        if ($char == '{' || $char == '[') {
-          $pos++;
-        }
-        for ($j = 0; $j < $pos; $j++) {
-          $result .= $indentStr;
-        }
-      }
-      $prevChar = $char;
-    }
+			// If the last character was the beginning of an element,
+			// output a new line and indent the next line
+			if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+				$result .= $newLine;
+				if ($char == '{' || $char == '[') {
+					$pos++;
+				}
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+			$prevChar = $char;
+		}
 
-    return $result;
-  }
+		return $result;
+	}
 }
